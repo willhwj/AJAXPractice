8b54baeb6c86b0a1f76215ebfdceee7a06521ba8
willhwj <williamhuwj@gmail.com>
2021-09-02 03:14:43 +0000

    William ans

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,2 +1,13 @@
 
-let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+let url = " https://api.kanye.rest/"
+
+window.addEventListener('DOMContentLoaded', function(){
+    let btn=document.querySelector('#btn');
+    console.log(btn);
+    document.addEventListener('click', async function(){
+        let response = await axios.get(url);
+        console.log(response.data.quote);
+        let quote=response.data.quote;
+        document.querySelector('#quote').innerHTML=quote;
+    })
+})
